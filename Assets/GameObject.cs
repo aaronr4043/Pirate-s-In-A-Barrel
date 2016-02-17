e7eee3a3caa2a537b89cc3e72ee86286804e4a16
aaronr4043 <aaronr4043@gmail.com>
2016-02-17 12:32:19 +0000

    testing Classes

---
+++ Assets/GameObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObject : MonoBehaviour 
+{
+
+	public class Objects
+	{
+		public float speed;
+		public float x;
+		public float y;
+		public float z;
+		public float healthpoints;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
