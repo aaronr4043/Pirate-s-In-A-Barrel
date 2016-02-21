c7d0af6c35044d464a1b5350e320f5c388d1e1ee
Paul Redmond <paulredmond1994@gmail.com>
2016-02-21 16:59:59 +0000

    Added files via upload
    Pauls Stuff
    Added Pathfinding

---
+++ tutorial/Assets/setColor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setColor : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = Color.red;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
