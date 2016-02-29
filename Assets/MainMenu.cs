5f6619362cdefd86ea935126c4dfe85c99fe14aa
aaronr4043 <aaronr4043@gmail.com>
2016-02-29 18:07:43 +0000

    Added Options Menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public bool isPlay;
+    public bool isHelp;
+    public bool isOptions;
+    public bool isExit;
+    
+	void OnMouseUP()
+    {
+        if(isPlay)
+        {
+            Application.LoadLevel(3);
+        }
+
+        if (isHelp)
+        {
+            Application.LoadLevel(2);
+        }
+
+        if (isOptions)
+        {
+            Application.LoadLevel(1);
+        }
+
+        if (isExit)
+        {
+            Application.Quit();
+        }
+    }
+}
