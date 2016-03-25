4cab0665cc01aaf026f1f801646d36e8cc1515a4
Irish-Day-Man <paulredmond1994@gmail.com>
2016-03-25 17:36:05 +0000

    Hello

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -12,17 +12,29 @@ public class MainMenu : MonoBehaviour
     {
         if(isPlay)
         {
+<<<<<<< HEAD
+            Application.LoadLevel(1);
+=======
             Application.LoadLevel(3);
+>>>>>>> 8d6141437069199a0f1677ff7af16505bc8d01fd
         }
 
         if (isHelp)
         {
+<<<<<<< HEAD
+            Application.LoadLevel(3);
+=======
             Application.LoadLevel(2);
+>>>>>>> 8d6141437069199a0f1677ff7af16505bc8d01fd
         }
 
         if (isOptions)
         {
+<<<<<<< HEAD
+            Application.LoadLevel(0);
+=======
             Application.LoadLevel(1);
+>>>>>>> 8d6141437069199a0f1677ff7af16505bc8d01fd
         }
 
         if (isExit)
--- Assets/UIManagerScript.cs
+++ Assets/UIManagerScript.cs
@@ -8,11 +8,17 @@ public class UIManagerScript : MonoBehaviour
         Application.LoadLevel(0);
     }
 
+<<<<<<< HEAD
+    public void PlayGame()
+=======
     public void GoOptions()
+>>>>>>> 8d6141437069199a0f1677ff7af16505bc8d01fd
     {
         Application.LoadLevel(1);
     }
 
+<<<<<<< HEAD
+=======
     public void GoHelp()
     {
         Application.LoadLevel(2);
@@ -23,6 +29,7 @@ public class UIManagerScript : MonoBehaviour
         Application.LoadLevel(3);
     }
 
+>>>>>>> 8d6141437069199a0f1677ff7af16505bc8d01fd
     public void ExitGame()
     {
         Application.Quit();
